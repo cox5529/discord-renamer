02f6df160b3c22954462b688830c30d1b02bf379
Brandon Cox <brandon.cox@metova.com>
2022-02-13 20:09:19 -0600

    Trim secrets

--- src/docker-secrets.ts
+++ src/docker-secrets.ts
@@ -11,6 +11,6 @@ export function config(dir?: string) {
   for (const file of files) {
     const value = fs.readFileSync(`${dir}/${file}`);
 
-    process.env[file] = value.toString('utf-8');
+    process.env[file] = value.toString('utf-8').trim();
   }
 }
